8b159111bf1ad8dfec36a1e2a3675c923ee48c1b
leonelentes <33723627+leonelentes@users.noreply.github.com>
2018-03-07 16:02:40 +0800

    src/app/notes-detail/note-detail.component.html

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { UserLoginComponent } from './ui/user-login/user-login.component';
+import { ItemsListComponent } from './items/items-list/items-list.component';
+import { ReadmePageComponent } from './ui/readme-page/readme-page.component';
+import { NotesListComponent } from './notes/notes-list/notes-list.component';
+
+import { AuthGuard } from './core/auth.guard';
+import { CoreModule } from './core/core.module';
+
+const routes: Routes = [
+  { path: '', component: NotesListComponent, canActivate:[AuthGuard] },
+  { path: 'login', component: UserLoginComponent },
+  { path: 'items', component: ItemsListComponent, canActivate: [AuthGuard] },
+  { path: 'notes', component: NotesListComponent,  canActivate: [AuthGuard] },
+  // uploads are lazy loaded
+  { path: 'uploads', loadChildren: './uploads/shared/upload.module#UploadModule', canActivate: [AuthGuard] },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [AuthGuard],
+})
+export class AppRoutingModule { }
